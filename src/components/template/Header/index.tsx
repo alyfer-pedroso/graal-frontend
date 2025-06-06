5d8f5360ac57995cf8a0c87355328fa685ef932c
Alyfer Pedroso <alyferleandroaraujopedroso@gmail.com>
2025-06-06 03:47:55 -0300

    minor fixes

--- src/components/template/Header/index.tsx
+++ src/components/template/Header/index.tsx
@@ -14,7 +14,7 @@ interface props {
 
 export const Header: FC<props> = ({ show }) => {
   const { currentRoute, PAGES_LIST } = usePagesList();
-  const inHome = useMemo(() => currentRoute?.title === PAGES_LIST[2]?.title, [currentRoute]);
+  const inHome = useMemo(() => currentRoute?.title === PAGES_LIST[2]?.title, [currentRoute?.title]);
 
   const headerComponent = useMemo(() => {
     if (inHome) {
--- src/data/hooks/usePagesList/index.ts
+++ src/data/hooks/usePagesList/index.ts
@@ -1,12 +1,16 @@
 import { useMemo } from "react";
+import { useLocation } from "react-router";
+
 import { PAGES_LIST } from "@/data/constants";
 import { UserRoles } from "@/data/models/roles";
 
 export function usePagesList() {
+  const { pathname } = useLocation();
+
   const hierarchy = useMemo(() => Number(localStorage.getItem("hierarquia")) as UserRoles, []);
   const routes = useMemo(() => PAGES_LIST.filter((page) => page.hierarchies.includes(hierarchy)), [hierarchy]);
 
-  const currentRoute = useMemo(() => routes.find(({ path }) => path.includes(window.location.hash.split("/")[1])), [window.location.hash]);
+  const currentRoute = useMemo(() => routes.find(({ path }) => path.includes(pathname.split("/")[1])), [pathname]);
 
   const routesForHome = useMemo(() => routes.filter(({ showInHome }) => showInHome), [routes]);
 
